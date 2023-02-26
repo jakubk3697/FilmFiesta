1e41624a50568167421ca54514a2727f0962f04e
jakubk3697 <jakubkaminski3697@gmail.com>
2023-02-26 04:08:56 +0100

    Matching  movies using AI works. Currently in console.

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { MainNavbar } from './MainNavbar';
 import { MovieCards } from './MovieCards';
-import { fetchMovies } from '../api/moviedbAPI';
-import { useInfiniteQuery } from '@tanstack/react-query';
-import { Route, Routes } from 'react-router-dom';
 import { RandomQuestions } from './RandomQuestions';
+import { fetchMovies } from '../api/moviedbAPI';
+import { getMoviesByAI } from '../api/openAIAPI';
 import styles from '../assets/styles/MainContent.module.scss';
 
 
@@ -13,31 +13,6 @@ export const MainContent = ({ type }) => {
     const [movieType, setMovieType] = useState('popular');
     const [prompt, setPrompt] = useState('');
     const [response, setResponse] = useState('');
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        const headers = {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
-        };
-
-        const data = {
-            'model': "text-davinci-003",
-            'prompt': prompt,
-            'temperature': 0.7,
-            'max_tokens': 500,
-        };
-
-        try {
-            const response = await axios.post('https://api.openai.com/v1/completions', data, { headers });
-            console.log('response', response);
-            const text = response.data.choices[0].text.trim();
-            setResponse(text);
-        } catch (error) {
-            console.error(error);
-        }
-    }
 
     // Initial fetch strucutre with page 1
     const fetchProjects = async ({ pageParam = 1 }) => {
@@ -60,12 +35,16 @@ export const MainContent = ({ type }) => {
         setMovieType(type)
     }
 
-    const handleAISeachbarSubmit = (event) => {
-        event.preventDefault();
-    }
-
     const handleAISeachbarChange = (event) => {
         setPrompt(event.target.value);
+    }
+
+    const handleAISeachbarSubmit = async (event) => {
+        event.preventDefault();
+
+        const response = await getMoviesByAI({ userPrompt: prompt });
+        setResponse(response);
+        setPrompt('');
     }
 
     const movieData = data ? data.pages.flatMap((page) => page.results) : [];
