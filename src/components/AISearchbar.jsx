7c225836dbe4bfe70da6af898edc9b93bc611c25
jakubk3697 <jakubkaminski3697@gmail.com>
2023-02-27 20:39:29 +0100

    Routing for ai button works

--- src/components/AISearchbar.jsx
+++ src/components/AISearchbar.jsx
@@ -1,16 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from '../assets/styles/AISearchbar.module.scss';
 
 export const AISearchbar = ({ onSubmit, aiPromptRef }) => {
+    let navigateTo = useNavigate();
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        onSubmit();
+        navigateTo('/movies/ai');
+    }
+
     return (
         <div className={styles.aiBox}>
-            <form className={styles.form} onSubmit={onSubmit}>
+            <form className={styles.form} onSubmit={onSubmit} method={'post'}>
                 <input className={styles.input} placeholder="Give some tips and get matched movies..." type="text" ref={aiPromptRef} />
-                {/* let below function to change route and submit form */}
-                
-                <Link to={'/movies/ai'}>
-                    <button className={styles.submitBtn} type="submit">Match movies</button>
-                </Link>
+                <button className={styles.submitBtn} type="submit" onClick={handleClick}>Match movies</button>
             </form>
         </div>
     )
