7d5c0bcd418c886890dc4b2e2ffed3d87fbfd847
jakubk3697 <jakubkaminski3697@gmail.com>
2023-03-01 21:55:14 +0100

    Inital structure and styling

--- src/api/moviedbAPI.jsx
+++ src/api/moviedbAPI.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_MOVIEDB_API_KEY;
 const API_ACTUAL_GENRES = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
-const BASE_URL = 'https://api.themoviedb.org/3/';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const fetchMovies = async ({ queryKey }) => {
     const [_key, { page, movieType }] = queryKey;
